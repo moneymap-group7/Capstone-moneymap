c70fa6b30810533aa518dcd5c72a2ffb83348521
hetvi <hetvikaswala151@gmail.com>
2026-01-31 02:08:03 -0500

    changes made to app modules  and schema prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
-
