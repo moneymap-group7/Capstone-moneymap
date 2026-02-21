d14f57d34036e7b73b2a3e7237e3591e6f90618e
Het <hetharani87@gmail.com>
2026-02-21 15:20:50 -0500

    register UtilizationController in module and finalize alert integration wiring

--- backend/src/budgets/utilization/utilization.module.ts
+++ backend/src/budgets/utilization/utilization.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 import { PrismaModule } from "../../prisma/prisma.module";
 import { UtilizationService } from "./utilization.service";
 import { AlertsService } from "../../common/alerts/alerts.service";
+import { UtilizationController } from "./utilization.controller";
 
 @Module({
   imports: [PrismaModule],
+  controllers: [UtilizationController],
   providers: [UtilizationService, AlertsService],
   exports: [UtilizationService],
 })
