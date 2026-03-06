40a94d4dcb78ece561fdd15e5c5728fb6546b5e4
rudra1707 <160192262+rudra1707@users.noreply.github.com>
2026-03-06 14:06:01 -0500

    Integrated user rules into transaction and statement categorization (#97)

--- backend/src/common/common.module.ts
+++ backend/src/common/common.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
 import { AutoCategorizeService } from "./categorization/auto-categorize.service";
+import { CategoryResolverService } from "./categorization/category-resolver/category-resolver.service";
+import { RulesModule } from "../rules/rules.module";
 
 @Module({
-  providers: [AutoCategorizeService],
-  exports: [AutoCategorizeService],
+  imports: [RulesModule],
+  providers: [AutoCategorizeService, CategoryResolverService],
+  exports: [AutoCategorizeService, CategoryResolverService],
 })
 export class CommonModule {}
--- backend/src/rules/rules.module.ts
+++ backend/src/rules/rules.module.ts
@@ -2,11 +2,12 @@ import { Module } from "@nestjs/common";
 import { PrismaModule } from "../prisma/prisma.module";
 import { RulesController } from "./rules.controller";
 import { RulesService } from "./rules.service";
+import { RuleEngineService } from "./rule-engine/rule-engine.service";
 
 @Module({
   imports: [PrismaModule],
   controllers: [RulesController],
-  providers: [RulesService],
-  exports: [RulesService],
+  providers: [RulesService, RuleEngineService],
+  exports: [RulesService, RuleEngineService],
 })
 export class RulesModule {}
